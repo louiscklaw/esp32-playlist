54cac1819b51d62975d2c7ca671cb3c2d291a53e
Louis Law <louiscklaw@users.noreply.github.com>
2022-02-25 16:16:45 +0800

    update arduino-blink,

--- firmware/m5stack/TFT_eSPI/TFT_eSPI_helloworld/src/main.cpp
+++ firmware/m5stack/TFT_eSPI/TFT_eSPI_helloworld/src/main.cpp
@@ -1,11 +1,65 @@
 
 #include <Arduino.h>
+
 #include <SPI.h>
+#include <TFT_eSPI.h>
 
 #include <Foo.h>
 
+TFT_eSPI tft = TFT_eSPI();
+
+// TFT_eSPI setup
+
+// This is the file name used to store the touch coordinate
+// calibration data. Change the name to start a new calibration.
+#define CALIBRATION_FILE "/TouchCalData3"
+
+// Set REPEAT_CAL to true instead of false to run calibration
+// again, otherwise it will only be done once.
+// Repeat calibration if you change the screen rotation.
+#define REPEAT_CAL false
+
+bool SwitchOn = false;
+
+// Comment out to stop drawing black spots
+#define BLACK_SPOT
+
+// Switch position and size
+#define FRAME_X 100
+#define FRAME_Y 64
+#define FRAME_W 120
+#define FRAME_H 50
+
+// Red zone size
+#define REDBUTTON_X FRAME_X
+#define REDBUTTON_Y FRAME_Y
+#define REDBUTTON_W (FRAME_W/2)
+#define REDBUTTON_H FRAME_H
+
+// Green zone size
+#define GREENBUTTON_X (REDBUTTON_X + REDBUTTON_W)
+#define GREENBUTTON_Y FRAME_Y
+#define GREENBUTTON_W (FRAME_W/2)
+#define GREENBUTTON_H FRAME_H
+
+// TFT_eSPI setup
+
+void touch_calibrate();
+
 void setup()
 {
+  Serial.begin(9600);
+  tft.init();
+
+  // Set the rotation before we calibrate
+  tft.setRotation(1);
+
+  // call screen calibration
+  touch_calibrate();
+
+  // clear screen
+  tft.fillScreen(TFT_BLUE);
+
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
@@ -15,4 +69,10 @@ void loop()
   delay(1000);
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
+}
+
+
+void touch_calibrate()
+{
+
 }
